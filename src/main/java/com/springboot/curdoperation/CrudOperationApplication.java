5d9801bfad36a4257a936864a0ee80ed9b2c5166
SARTHAK\sarth <sarthakkalbande555@gmail.com>
2024-05-05 11:20:09 +0530

    SpringBootStructure- changed the application name

--- src/main/java/com/springboot/curdoperation/CrudOperationApplication.java
+++ src/main/java/com/springboot/curdoperation/CrudOperationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CurdOperationApplication {
+public class CrudOperationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CurdOperationApplication.class, args);
+		SpringApplication.run(CrudOperationApplication.class, args);
 	}
 
 }
