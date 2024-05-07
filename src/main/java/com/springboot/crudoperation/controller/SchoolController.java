a64522785e2788901c949d1201615de2dd75f17a
SARTHAK\sarth <sarthakkalbande555@gmail.com>
2024-05-07 14:23:55 +0530

    SpringBootStructure- created RestFUL APIs for Crud operations using responseDto

--- src/main/java/com/springboot/crudoperation/controller/SchoolController.java
+++ src/main/java/com/springboot/crudoperation/controller/SchoolController.java
@@ -1,14 +1,18 @@
 package com.springboot.crudoperation.controller;
 
+import com.springboot.crudoperation.model.ResponseDto;
 import com.springboot.crudoperation.model.School;
 import com.springboot.crudoperation.service.SchoolService;
 import com.springboot.crudoperation.service.impl.SchoolServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name="School Controller", description = "This is used to perform crud operations on school data")
 @RestController
 public class SchoolController {
 
@@ -21,16 +25,23 @@ public class SchoolController {
        return schoolService.findSchoolById(1);
     }
 
+    @Operation(summary="Fetching school data using school Id")
     @GetMapping(value = "/school1")
     public ResponseEntity<?> getValueByResponseEntity(){
 
         return new ResponseEntity<>(schoolService.findSchoolById(2), HttpStatus.OK);
     }
 
-    @GetMapping(value = "/school/{schoolId}")
-    public ResponseEntity<?> getValueUsingPV(@PathVariable int schoolId){
+//    @GetMapping(value = "/school/{schoolId}")
+//    public ResponseEntity<?> getValueUsingPV(@PathVariable int schoolId){
+//
+//        return new ResponseEntity<>(schoolService.findSchoolById(schoolId), HttpStatus.OK);
+//    }
 
-        return new ResponseEntity<>(schoolService.findSchoolById(schoolId), HttpStatus.OK);
+    @GetMapping(value = "/school/{schoolId}")
+    public ResponseEntity<?> getSchoolUsingRespDto(@PathVariable int schoolId) {
+
+        return new ResponseEntity<>(ResponseDto.builder().statusCode(HttpStatus.OK.value()).message("s").data(schoolService.findSchoolById(schoolId)).build(),HttpStatus.OK);
     }
 
     @GetMapping(value = "/schooll")
